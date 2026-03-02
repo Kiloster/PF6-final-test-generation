7148c841103b19684d1daa4cc9a255c099f6281c
Kiloster <estebanalejandrom@gmail.com>
2026-03-02 12:42:37 -0500

    Solución de lógica API y funciones para platos típicos

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import json
+import requests
+
+def dish_fetch(num):
+    url = f"https://api-colombia.com/api/v1/TypicalDish/{num}"
+    
+    response = requests.get(url)    
+    plato = response.json()
+    
+    return plato
+
 def main():
-  print("Hello learners!")
+  
+    print("🍲 --- EXPLORADOR DE GASTRONOMÍA COLOMBIANA --- 🍲")
+    
+    id_plato = input("Introduce el ID del plato: ")
+    
+    
+    resultado = dish_fetch(id_plato)
+    
+    
+    print(f"Resultado: {resultado['name']}")
 
 if __name__=="__main__":
   main()
